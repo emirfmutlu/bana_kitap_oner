84ff4aee42c179caa110ce755891656c4bfe59b0
emirfmutlu <emirfmutlu@gmail.com>
2022-01-12 19:04:07 +0300

    Added flask templating + some styling changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for
 
 app= Flask(__name__)
 
@@ -7,6 +7,8 @@ app= Flask(__name__)
 def home():
     return render_template('index.html')
 
-
+@app.route('/secim')
+def secim():
+    return render_template('form.html')
 if __name__ == '__main__':
     app.run(debug=True)
